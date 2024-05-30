5313587e6b6fba281c4a17efdfb6140fa26d521c
Mihaela Angelova <mihaela.yankova7@gmail.com>
2024-05-30 09:58:12 +0300

    Create folder hierarchy in the backend.

--- backend/db.js
+++ backend/db.js
@@ -8,31 +8,4 @@ const pool = new Pool({
     database: "readaer_db"
 })
 
-const createTableQuery = `CREATE TABLE "user" (
-    user_id serial PRIMARY KEY,
-    email VARCHAR (50) UNIQUE NOT NULL,
-    password VARCHAR (50) UNIQUE NOT NULL,
-    first_name VARCHAR (50) NOT NULL,
-    last_name VARCHAR (50) NOT NULL,
-    country VARCHAR (50) UNIQUE NOT NULL,
-    gender VARCHAR (1) NOT NULL,
-    date_of_birth DATE
-);`
-
-pool.query(createTableQuery)
-.then((response) => {
-    console.log("Table created");
-    console.log(response);
-})
-.catch((err) => {
-    if(err.code === '42P04') { // 42P04 is the error code for 'database already exists'
-        console.log("Database already exists.");
-    } else {
-        console.error("Error creating database: ", err);
-    }
-})
-.finally(() => {
-    pool.end(); // close the pool connection
-});
-
 module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,23 @@ const app = express()
 app.use(express.json()); //interpret json requests
 app.use(cors());
 
-app.post("/adduser", (req, res) => {
+app.post("/register", (req, res) => {
+    const email = req.body["email"]
+    const password = req.body["password"]
+
+    console.log("Email: " + email);
+    console.log("Password: " + password);
+
+    const insertStatement = `INSERT INTO "readaer_db" (email, password) VALUES ("${email}", "${password}");`
+
+    pool.query(insertStatement).then((response) => {
+        console.log("Data saved");
+        console.log(response);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
     console.log(req.body);
     res.send("Respond received: " + req.body);
 });
